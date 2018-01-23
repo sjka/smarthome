ef1ddce913085656ecf1ef94378bab344897dc7a
Simon Kaufmann <simon.kfm@googlemail.com>
2018-01-23 15:20:01 +0100

    added test-case
    Signed-off-by: Simon Kaufmann <simon.kfm@googlemail.com>

--- bundles/ui/org.eclipse.smarthome.ui.test/src/test/java/org/eclipse/smarthome/ui/internal/proxy/ProxyServletServiceTest.java
+++ bundles/ui/org.eclipse.smarthome.ui.test/src/test/java/org/eclipse/smarthome/ui/internal/proxy/ProxyServletServiceTest.java
@@ -12,16 +12,24 @@
  */
 package org.eclipse.smarthome.ui.internal.proxy;
 
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.eclipse.jetty.client.api.Request;
 import org.eclipse.jetty.http.HttpHeader;
 import org.eclipse.jetty.util.B64Code;
 import org.eclipse.jetty.util.StringUtil;
+import org.eclipse.smarthome.core.library.types.StringType;
+import org.eclipse.smarthome.model.core.ModelRepository;
+import org.eclipse.smarthome.model.sitemap.Image;
+import org.eclipse.smarthome.model.sitemap.Sitemap;
+import org.eclipse.smarthome.ui.items.ItemUIRegistry;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -64,5 +72,29 @@ public class ProxyServletServiceTest {
         URI uri = new URI("http://127.0.0.1:8080/content");
         service.maybeAppendAuthHeader(uri, request);
         verify(request, never()).header(any(HttpHeader.class), anyString());
+    }
+
+    @Test
+    public void testUriFromRequest_stateNotURL() {
+        ModelRepository modelRepository = mock(ModelRepository.class);
+        ItemUIRegistry itemUIRegistry = mock(ItemUIRegistry.class);
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        Sitemap sitemap = mock(Sitemap.class);
+        Image widget = mock(Image.class);
+
+        service.setModelRepository(modelRepository);
+        service.setItemUIRegistry(itemUIRegistry);
+
+        when(request.getParameter(eq("sitemap"))).thenReturn("testSitemap");
+        when(request.getParameter(eq("widgetId"))).thenReturn("testWidget");
+        when(modelRepository.getModel(eq("testSitemap"))).thenReturn(sitemap);
+        when(itemUIRegistry.getWidget(eq(sitemap), eq("testWidget"))).thenReturn(widget);
+        when(widget.getUrl()).thenReturn("http://www.eclipse.org");
+        when(widget.getItem()).thenReturn("testItem");
+        when(itemUIRegistry.getItemState(eq("testItem"))).thenReturn(new StringType("5"));
+
+        URI res = service.uriFromRequest(request);
+        assertNotNull(res);
+        assertEquals(res.toString(), "http://www.eclipse.org");
     }
 }
